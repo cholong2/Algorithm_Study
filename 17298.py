771b4dcc3872896bf94983cd546815a241fd1562
cholong2 <pcl106@naver.com>
2021-08-19 15:07:52 +0900

    오큰수(내방식으로 다시 풀어냈다!)

--- 17298.py
+++ 17298.py
@@ -5,17 +5,15 @@ import sys
 # 방법2 stack을 이용한다면? =>
 
 num = int(input())
-a = list(map(int, input().split(" ")))
-result = ["-1" for _ in range(num)]
+a = list(map(int, input().split()))
+big_num = [-1] * num
 stack = []
-stack.append(0)
-i = 1
+
+
 for i in range(num):
     while stack and a[stack[-1]] < a[i]:
-        result[stack[-1]] = str(a[i])
-        stack.pop()
-
+        big_num[stack.pop()] = a[i]
     stack.append(i)
-    i += 1
 
-print(" ".join(result))
+for i in range(num):
+    print(big_num[i], end = " ")
