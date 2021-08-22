fac44c45de720beb2139698312ba2beed3b75b05
cholong2 <pcl106@naver.com>
2021-08-22 21:03:41 +0900

    후위 표기식 성공~

--- 1918_후위 표기식.py
+++ 1918_후위 표기식.py
@@ -2,8 +2,8 @@
 # 중위 표기식을 후위 표기식으로 바꾸는 거
 # 이것도 스택 쓰면 되겠지!
 # 우선순위 1 (), 2 */, 3 +- 로 조건문 나누기
-import sys
-a = list(sys.stdin.readline().rstrip())
+
+a = list(input())
 temp = []
 result = ''
 
@@ -11,21 +11,24 @@ result = ''
 for i in a:
     #문자면 바로 출력
     if i.isalpha():
-        result+=(i)
+        result += i
     else:
         if i == '(':
             temp.append(i)
+
         elif i == ')':
-            while temp and  temp[-1] != '(':
+            while temp and temp[-1] != '(':
                 result+=temp.pop()
             temp.pop()
+
         elif i == '*' or i == '/':
-            while temp and (temp[-1] == '*' and temp[-1] == '/'):
-                result=+temp.pop()
+            while temp and (temp[-1] == '*' or temp[-1] == '/'):
+                result+=temp.pop()
             temp.append(i)
+
         elif i == '+' or i == '-':
             while temp and temp[-1] != '(':
-                result =+temp.pop()
+                result +=temp.pop()
             temp.append(i)
 
 while temp:
