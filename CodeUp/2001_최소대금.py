d7dc2c632e67b5e11e5ceef4d1f9223e4928af96
cholong2 <pcl106@naver.com>
2022-03-15 17:44:10 +0900

    코드업_2001 최소대금

---
+++ CodeUp/2001_최소대금.py
@@ -0,0 +1,19 @@
+#2001 : 최소대금
+
+arr = []
+n=5
+
+for i in range(n):
+    arr.append(int(input()))
+
+p = arr[:3]
+j = arr[3:]
+
+
+p_min = min(p)
+j_min = min(j)
+
+result = (p_min+j_min)*1.1
+
+result = round(result, 1)
+print (result)
