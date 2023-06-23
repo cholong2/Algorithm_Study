566f6009f7c696083a7506586ce78109d4d8e466
cholong2 <80766477+cholong2@users.noreply.github.com>
2023-06-24 00:07:44 +0900

    완전탐색

---
+++ Programmers/Level2/최소직사각형.java
@@ -0,0 +1,21 @@
+package Level2;
+import java.util.*;
+//완전탐색
+
+public class 최소직사각형 {
+
+    class Solution {
+        public int solution(int[][] sizes) {
+            int answer = 0;
+            int r= 0, c = 0;
+
+            for (int[] card : sizes){
+                r = Math.max(r, Math.max(card[0], card[1]));
+                c = Math.max(c, Math.min(card[0], card[1]));
+            }
+            answer = r*c;
+            return answer;
+
+        }
+    }
+}
