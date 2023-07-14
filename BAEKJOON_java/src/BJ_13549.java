1c0eead7dda723b401364d4dd6f7dc173297a26c
cholong2 <80766477+cholong2@users.noreply.github.com>
2023-07-14 17:32:15 +0900

    BFS
    배열 생성해서 큐에 추가하는거는 안됨
    클래스 생성해서 추가하는거는 된다~ 왜 와이~?

--- BAEKJOON_java/src/BJ_13549.java
+++ BAEKJOON_java/src/BJ_13549.java
@@ -5,53 +5,58 @@ End : 23.04.18:55
  */
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class BJ_13549 {
 
-    static int n, k, x, temp = 0;
-    static int[] check;
-    static Deque<Integer> q = new LinkedList<Integer>();
+    static int n, k= 0;
+    static int[] visited = new int[100001];
+    static Queue<Loc> q = new LinkedList<>();
+    static class Loc{
+        int idx;
+        int time;
 
+        public Loc(int idx, int time) {
+            this.idx = idx;
+            this.time = time;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
+        q.add(new Loc(n,1));
+        visited[n] = 1;
 
-        check = new int[100001];
-        Arrays.fill(check, -1);
-        x = n;
-        q.add(n);
-        check[n] = 0;
+       while (!q.isEmpty()){
+           Loc now = q.poll();
 
+           if (now.idx+1>=0 && now.idx+1<=100000){
+               if (visited[now.idx+1] == 0 || visited[now.idx+1] > now.time+1){
+                   visited[now.idx+1] = now.time+1;
+                   q.add(new Loc (now.idx+1, now.time+1));
+               }
+           }
 
-        while (!q.isEmpty()) {
-            temp = q.poll();
-            if (temp == k) {
-                break;
-            }
-            if (temp * 2 <= 100000 && check[temp * 2] < 0) {
-                q.addFirst(temp * 2);
-                check[temp * 2] = check[temp];
-            }
-            if (temp + 1 <= 100000 && check[temp + 1] < 0) {
-                check[temp + 1] = check[temp] + 1;
-                q.addLast(temp + 1);
-            }
+           if (now.idx-1>=0 && now.idx-1<=100000){
+               if (visited[now.idx-1] == 0 || visited[now.idx-1] > now.time+1){
+                   visited[now.idx-1] = now.time+1;
+                   q.add(new Loc(now.idx-1, now.time+1));
+               }
+           }
 
-            if (temp - 1 >= 0 && check[temp - 1] < 0) {
-                check[temp - 1] = check[temp] + 1;
-                q.addLast(temp - 1);
-                // System.out.println("이프문3"+ q.size());
-            }
+           if (now.idx*2>=0 && now.idx*2<=100000){
+               if (visited[now.idx*2] == 0 || visited[now.idx*2] > now.time){
+                   visited[now.idx*2] = now.time;
+                   q.add(new Loc(now.idx*2, now.time));
+               }
+           }
 
-        }
-        System.out.println(check[k]);
+       }
 
+       System.out.println(visited[k]-1);
 }
 }
