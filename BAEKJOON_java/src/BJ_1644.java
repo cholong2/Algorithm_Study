9007ed4e1a562fad0b279d5fe8765355f4a72b6b
cholong2 <80766477+cholong2@users.noreply.github.com>
2023-11-14 18:34:32 +0900

    수

--- BAEKJOON_java/src/BJ_1644.java
+++ BAEKJOON_java/src/BJ_1644.java
@@ -2,10 +2,13 @@
  * 백준 1644번 : 소수의 연속합
  * start : 2023-11-14 16:53
  * end : 2023-11-14
- * 알고리즘 : DP
+ * 알고리즘 : 투포인터, 소수
  * 한줄평 :
  * 1~n까지 에라토스테네츠 어쩌고 그거로 소수 판별하고? 그중에서 n을 만들 수 있는 연속합 찾기
- * 이분탐색? 으로
+ * 이분탐색?이 아니라 투포인터
+ * 에라토스테네츠의 채? 소수인지 찾고 내가 소수라면 배수들 모두 쳐내기
+ * 훠어어어ㅓㅇㄹ씬 빠름여
+ *
  */
 
 import java.io.*;
@@ -13,18 +16,27 @@ import java.util.*;
 
 public class BJ_1644 {
     static int n;
-    static ArrayList<Integer> prime = new ArrayList<>();
+   static ArrayList<Integer> primeArr = new ArrayList<>();
+    public static boolean[] prime;
     public static void main (String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(bf.readLine());
-
-        for (int i = 2 ; i <= n ; i++){
-            if (isPrime(i)){
-                prime.add(i);
+        prime = new boolean[n+1];
+        Arrays.fill(prime, true);
+        for (int i = 2 ; i*i <=n ; i++){
+            for (int j = i*2 ; j <= n ; j+=i){
+                if (prime[j]){
+                    prime[j] = false;
+                }
             }
         }
-        int size = prime.size();
-        prime.add(0);
+        for (int i = 2 ; i <= n ; i++){
+            if (prime[i]){
+                primeArr.add(i);
+            }
+        }
+        int size = primeArr.size();
+        primeArr.add(0);
         int s = 0;
         int e = 0;
         int sum = 0;
@@ -44,11 +56,11 @@ public class BJ_1644 {
 
         while (e <= size && s <= size){
             if (sum<n){
-                sum+=prime.get(e);
+                sum+=primeArr.get(e);
                 e++;
             }
             else {
-                sum-=prime.get(s);
+                sum-=primeArr.get(s);
                 s++;
             }
             if (sum==n) cnt++;
